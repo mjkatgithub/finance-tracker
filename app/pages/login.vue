8acc1bf929e3b1a30e8bb92efcdb59fa56a48fff
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-02-02 18:37:49 +0100

    add login.vue

---
+++ app/pages/login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <UCard v-if="!success">
+      <template #header>
+        Sign-in to Finance Tracker
+      </template>
+
+      <form @submit.prevent="handleLogin">
+        <UFormGroup label="Email" name="email" class="mb-4" :required="true"
+        help="You will receive an email with the confirmation link">
+          <UInput type="email" placeholder="Email" required v-model="email" />
+        </UFormGroup>
+
+        <UButton type="submit" variant="solid" color="black" :loading="pending" :disabled="pending">Sign-in</UButton>
+      </form>
+    </UCard>
+    <UCard v-else>
+      <template #header>
+        Email has been send
+      </template>
+
+      <div class="text-center">
+        <p class="mb-4">We have send an emai to <strong>{{ email }}</strong> with a link to sign-in.</p>
+        <p>
+          <strong>Important:</strong> The link will expire in 5 minutes.
+        </p>
+      </div>
+    </UCard>
+  </div>
+</template>
+
+<script setup>
+const success = ref(false)
+const email = ref('')
+const pending = ref(false)
+const toast = useToast()
+const supabase = useSupabaseClient()
+
+const handleLogin = async () => {
+  pending.value = true
+
+  try {
+    const {error} = await supabase.auth.signInWithOtp({
+      email: email.value,
+      Options:{
+        emailRedirectTo: 'http://localhost:3000'
+      }
+    })
+
+    if(error) {
+      toast.add({
+        title: 'Error authenticating',
+        icon: 'i-heroicons-exlamation-circle',
+        description: error.message,
+        color: 'red'
+      })
+    } else {
+      success.value = true
+    }
+  } finally {
+    pending.value = false
+  }
+}
+</script>
