e7b715ce2365a88a148796d0e6ad9c633b6ebf16
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-02-15 16:20:26 +0100

    add useAvatarUrl

--- app/pages/settings/avatar.vue
+++ app/pages/settings/avatar.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="mb-4">
       <UFormGroup label="Current avatar" class="w-full" help="This would be blank by default">
-        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" size="3xl" />
+        <UAvatar :src="url" size="3xl" />
       </UFormGroup>
     </div>
 
@@ -22,6 +22,8 @@
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 const { toastSuccess, toastError } = useAppToast()
+const {url} = useAvatarUrl()
+
 const uploading = ref(false)
 const fileInput = ref()
 
