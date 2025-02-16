9396263d84aeb8e9c6280ed732c8285be67e9dab
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-02-16 22:47:05 +0100

    make transaktions editable

--- app/components/transaction.vue
+++ app/components/transaction.vue
@@ -15,6 +15,7 @@
       <div>
         <UDropdown :items="items" :popper="{placement: 'bottom-start'}">
           <UButton color="white" variant="ghost" trailing-icon="i-heroicons-ellipsis-horizontal" :loading="isLoading" />
+          <TransactionModal v-model="isOpen" :transaction="transaction" @saved="emit('edited')" />
         </UDropdown>
       </div>
     </div>
@@ -25,7 +26,7 @@
 const props = defineProps({
   transaction: Object
 })
-const emit = defineEmits(['deleted'])
+const emit = defineEmits(['deleted', 'edited'])
 const isIncome = computed(() => props.transaction.type === 'Income')
 const icon = computed(
   () => isIncome.value ? 'i-heroicons-arrow-up-right' : 'i-heroicons-arrow-down-left'
@@ -39,6 +40,8 @@ const { currency } = useCurrency(props.transaction.amount)
 const isLoading = ref(false)
 const {toastError, toastSuccess} = useAppToast()
 const supabase = useSupabaseClient()
+
+const isOpen = ref(false)
 
 const deleteTransaction = async () => {
   isLoading.value = true
@@ -64,7 +67,7 @@ const items = [
     {
       label: 'Edit',
       icon: 'i-heroicons-pencil-square-20-solid',
-      click: () => console.log('Edit')
+      click: () => isOpen.value = true
     },
     {
       label: 'Delete',
